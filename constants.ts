5742e616c706aa428025b8c1f1eb50d3c60138b1
Jithin Lal K <jithinlalk25@gmail.com>
2024-09-10 19:32:00 +0530

    Update version and backend url

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const Constant = {
-  APP_VERSION: 8,
+  APP_VERSION: 9,
   // API_URL: "http://192.168.1.4:3000/",
   // API_URL: "https://rate-india-dev-phx7h.ondigitalocean.app/",
-  API_URL: "https://sea-turtle-app-as5mq.ondigitalocean.app/",
+  API_URL: "https://rate-india-prod-wgrmy.ondigitalocean.app/",
 };
