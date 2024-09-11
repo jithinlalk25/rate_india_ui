0fc5ace8d083b6f7873609c12b5d255f9ae00640
Jithin Lal K <jithinlalk25@gmail.com>
2024-09-11 17:10:28 +0530

    Update version

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
     name: getAppName(),
     slug: "rate_india_ui",
     scheme: "rate-india-ui",
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
